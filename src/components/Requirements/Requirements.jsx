460ba53ff572c3afc89f73071d93afa2d4156ce7
ihsanrabbani <ihsanrabbani25@gmail.com>
2021-07-18 18:48:21 +0700

    create onetap component

---
+++ src/components/Requirements/Requirements.jsx
@@ -0,0 +1,10 @@
+import '../styles.css'
+
+export const Requirements = () => {
+  return (
+    <div>
+      <h1 className="text-red m-0">Hello world</h1>
+      <p>Mantap beudh</p>
+    </div>
+  )
+}
---
+++ src/components/Requirements/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles.css'
+
+export const Requirements = () => {
+  return (
+    <div>
+      <h1 className="text-red m-0">Hello world</h1>
+      <p>Mantap beudh</p>
+    </div>
+  )
+}
